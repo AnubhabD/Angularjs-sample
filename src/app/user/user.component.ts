e4b19f1518c88bb8c78b37ba3b8cb65fb4142305
AnubhabD <anubhab.das@cbnits.com>
2022-07-29 12:12:27 +0530

    assignment 3

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -18,55 +18,55 @@ export class UserComponent implements OnInit {
   outputFromChild: EventEmitter<number> = new EventEmitter();
   num3!: number;
 
-  changeMethod(value: string): void {
-    if(value === 'add'){
-      this.add();
-    }
-    if(value === 'sub'){
-      this.sub();
-    }
-    if(value === 'multiply'){
-      this.multiply();
-    }
-    if(value === 'divide'){
-      this.divide();
-    }
-  }
-
-  add(): number {
-    this.num3 = this.num1 + this.num2;
-    this.outputFromChild.emit(this.num3);
-    return this.num3;
-  }
-
-  sub(): number {
-    this.num3 = this.num1 - this.num2;
-    this.outputFromChild.emit(this.num3);
-    return this.num3;
-  }
-
-  multiply(): number {
-    this.num3 = this.num1 * this.num2;
-    this.outputFromChild.emit(this.num3);
-    return this.num3;
-  }
-
-  divide(): number {
-    this.num3 = this.num1 / this.num2;
-    this.outputFromChild.emit(this.num3);
-    return this.num3;
-  }
-
-  // onClick(): number{
-  //   if(this.num1 > 0 && this.num2 > 0){
-  //     this.num3 = this.num1 * this.num2;
+  // changeMethod(value: string): void {
+  //   if(value === 'add'){
+  //     this.add();
   //   }
-  //   if(this.num1 < 0 || this.num2 < 0){
-  //     this.num3 = this.num1 / this.num2;
+  //   if(value === 'sub'){
+  //     this.sub();
   //   }
+  //   if(value === 'multiply'){
+  //     this.multiply();
+  //   }
+  //   if(value === 'divide'){
+  //     this.divide();
+  //   }
+  // }
+
+  // add(): number {
+  //   this.num3 = this.num1 + this.num2;
   //   this.outputFromChild.emit(this.num3);
   //   return this.num3;
   // }
+
+  // sub(): number {
+  //   this.num3 = this.num1 - this.num2;
+  //   this.outputFromChild.emit(this.num3);
+  //   return this.num3;
+  // }
+
+  // multiply(): number {
+  //   this.num3 = this.num1 * this.num2;
+  //   this.outputFromChild.emit(this.num3);
+  //   return this.num3;
+  // }
+
+  // divide(): number {
+  //   this.num3 = this.num1 / this.num2;
+  //   this.outputFromChild.emit(this.num3);
+  //   return this.num3;
+  // }
+
+  onClick(): number{
+    if(this.num1 > 0 && this.num2 > 0){
+      this.num3 = this.num1 * this.num2;
+    }
+    if(this.num1 < 0 || this.num2 < 0){
+      this.num3 = this.num1 / this.num2;
+    }
+    this.outputFromChild.emit(this.num3);
+    return this.num3;
+  }
 
   ngOnInit(): void {}
 }
